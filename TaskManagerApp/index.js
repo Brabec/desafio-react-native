3992032c0dbcf0a5e91afe33fee09d24ce3ecae6
Brabec <lu_brabec@hotmail.com>
2018-01-04 19:45:36 -0300

    Adding the starting project - TaskManagerApp

---
+++ TaskManagerApp/index.js
@@ -0,0 +1,10 @@
+import { AppRegistry } from 'react-native';
+//import App from './App';
+
+const App = () => {
+    return (
+        <Text>Ola</Text>
+    );
+};
+
+AppRegistry.registerComponent('TaskManagerApp', () => App);
